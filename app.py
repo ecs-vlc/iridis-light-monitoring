d98a72882a68df2dc75adb240dbfc07e0c9d6c02
Ieremie <ieremie.ioan99@yahoo.com>
2023-10-04 23:38:30 +0100

    removed port

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def index():
                            last_update_time=data['last_update_time'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', debug=False)
 
